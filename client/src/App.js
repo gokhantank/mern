04a0e962afd8fad206956584a49abc79a81c963c
Gokhan Tank <hello@gokhantank.com>
2019-02-09 15:30:24 +1100

    client folder cretead with react-app-creator and server.js has been added under backend folder

---
+++ client/src/App.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          Header
+        </header>
+        <div className="App-main">
+          Main
+        </div>
+        <div className="App-footer">
+          Footer
+        </div>
+      </div>
+    );
+  }
+}
+
+export default App;
